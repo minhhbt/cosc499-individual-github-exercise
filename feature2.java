b315b370c4068f86ef2ab0f498bb8d7dccdb8070
minhhbt <buithienminh@gmail.com>
2022-09-16 07:28:00 -0700

    Added code for feature2.

---
+++ feature2.java
@@ -0,0 +1,14 @@
+//Feature 1 - Multiply two input integers
+import java.util.Arrays;  
+import java.util.Scanner;
+import java.util.*;
+
+public class feature2 {
+    public static void main(String args[]) {  
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+    
+        System.out.println("Enter the first number: ");
+        System.out.println("Enter the second number: ");
+    } 
+}
